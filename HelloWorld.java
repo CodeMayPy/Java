d1014eb335d706a6d547654726a6c3acca21eac9
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-01-20 12:27:51 -0300

    primeiro commit

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String args []) {
+        System.out.println("Meu primeiro programa java: Hello world!");
+        System.out.println("Java é massa!");
+    }
+}
