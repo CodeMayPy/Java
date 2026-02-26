94906cf427fef8ca2f3bb130cbe058d4530df011
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-02-26 14:30:43 -0300

    Repetição for

---
+++ Exercícios_beecrowd/Ex1143.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+    public class Ex1143{
+        public static void main(String[] args){
+            Scanner teclado=new Scanner(System.in);
+
+            int numero = teclado.nextInt();
+
+            for (int num = 1; num <= numero; num++){
+                System.out.println(num + " " + (num * num) + " " + (num * num * num));
+            }
+
+            teclado.close();
+        }
+    }
